5eaf9346965afa7480dc48f91c68c13a7adb8fa8
Adil <adil_dostmohamed@yahoo.co.uk>
2016-08-17 22:50:15 +0100

    New js file to handle post header transitions on scroll

--- public/dist/js/main.min.js
+++ public/dist/js/main.min.js
@@ -4286,30 +4286,63 @@ $(document).ready(function() {
  }
 })(jQuery)
 
-var makeSticky = (function($){
+var changeStyle = (function($){
   return {
-    isFixed: function(target){
-      if($(target).length){
-        var offsetHeight = $(target).offset().top;
-        var targetHeight = $(target).outerHeight();
-        $(window).scroll(function() {
-          var scrollPos = $(window).scrollTop();
-          if(scrollPos >= offsetHeight){
-            $(target).addClass('is-fixed');
-            $('body').css({'padding-top': targetHeight});
-          } else {
-            $(target).removeClass('is-fixed');
-            $('body').css({'padding-top': 0});
-          }
-        });
-      } else {
-        return false;
-      }
+    isScrolling: function(targetScroll, targetDisplay, cssProperty, startCssValue, endCssValue, cssUnit){
+      //get the px value at which the css change should start
+      var targetOffsetHeight = $(targetScroll).offset().top;
+      // get the number of px for which the css change should take effect based on the height of the target element
+      var targetHeight = $(targetScroll).outerHeight();
+      //calculate the difference in css values from start to the end of the scroll
+      var cssDifference = endCssValue - startCssValue;
+      //calculate the change in css property per change in px of scroll
+      var changePerPx = cssDifference/targetHeight;
+      //fire the following event every time the window scroll event is fired
+      $(window).scroll(function() {
+        //get the scroll position in px from the top
+        var scrollPos = $(window).scrollTop();
+        //calculate the current scroll value minus any amount of offset from the top that needs to be applied if the target scrolling element is not at the top of the page
+        var scrollCssValue = scrollPos - targetOffsetHeight;
+        //fire the function below if the current scroll value is greater than the height at which the scroll css change should be applied
+        if(scrollPos >= targetOffsetHeight) {
+          //calculate the css value of the target element by adding in the expected css value for the given scroll point plus the starting css value for the element
+          $(targetDisplay).css(cssProperty, ((startCssValue + scrollCssValue * changePerPx) + cssUnit));
+        } else {
+          return false;
+        }
+      });
     }
   };
 })(jQuery);
 
-makeSticky.isFixed('.post-header__date-wrapper');
+changeStyle.isScrolling(".post-header", ".post-header", "opacity", 1, 0.5, "");
+changeStyle.isScrolling(".post-header", ".post-header__title", "opacity", 1, 0.2, "");
+changeStyle.isScrolling(".post-header", ".post-header__title", "top", 100, 50, "px");
+
+// var makeSticky = (function($){
+//   return {
+//     isFixed: function(target){
+//       if($(target).length){
+//         var offsetHeight = $(target).offset().top;
+//         var targetHeight = $(target).outerHeight();
+//         $(window).scroll(function() {
+//           var scrollPos = $(window).scrollTop();
+//           if(scrollPos >= offsetHeight){
+//             $(target).addClass('is-fixed');
+//             $('body').css({'padding-top': targetHeight});
+//           } else {
+//             $(target).removeClass('is-fixed');
+//             $('body').css({'padding-top': 0});
+//           }
+//         });
+//       } else {
+//         return false;
+//       }
+//     }
+//   };
+// })(jQuery);
+//
+// makeSticky.isFixed('.post-header__date-wrapper');
 
 tinymce.init({
   selector: '#post-body',
--- public/src/js/custom/sticky.js
+++ public/src/js/custom/sticky.js
@@ -1,24 +1,24 @@
-var makeSticky = (function($){
-  return {
-    isFixed: function(target){
-      if($(target).length){
-        var offsetHeight = $(target).offset().top;
-        var targetHeight = $(target).outerHeight();
-        $(window).scroll(function() {
-          var scrollPos = $(window).scrollTop();
-          if(scrollPos >= offsetHeight){
-            $(target).addClass('is-fixed');
-            $('body').css({'padding-top': targetHeight});
-          } else {
-            $(target).removeClass('is-fixed');
-            $('body').css({'padding-top': 0});
-          }
-        });
-      } else {
-        return false;
-      }
-    }
-  };
-})(jQuery);
-
-makeSticky.isFixed('.post-header__date-wrapper');
+// var makeSticky = (function($){
+//   return {
+//     isFixed: function(target){
+//       if($(target).length){
+//         var offsetHeight = $(target).offset().top;
+//         var targetHeight = $(target).outerHeight();
+//         $(window).scroll(function() {
+//           var scrollPos = $(window).scrollTop();
+//           if(scrollPos >= offsetHeight){
+//             $(target).addClass('is-fixed');
+//             $('body').css({'padding-top': targetHeight});
+//           } else {
+//             $(target).removeClass('is-fixed');
+//             $('body').css({'padding-top': 0});
+//           }
+//         });
+//       } else {
+//         return false;
+//       }
+//     }
+//   };
+// })(jQuery);
+//
+// makeSticky.isFixed('.post-header__date-wrapper');
